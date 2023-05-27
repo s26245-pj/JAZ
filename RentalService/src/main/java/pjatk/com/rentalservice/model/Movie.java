325ec016d9c90ddb5fd91d598e8ef08e54532674
s26245 <s26245@pjwstk.edu.pl>
2023-05-27 16:57:05 +0200

    commit

---
+++ RentalService/src/main/java/pjatk/com/rentalservice/model/Movie.java
@@ -0,0 +1,11 @@
+package pjatk.com.rentalservice.model;
+
+
+public class Movie {
+    private Long id;
+    private String title;
+    private String category;
+    private String description;
+    private int releaseYear;
+
+}
